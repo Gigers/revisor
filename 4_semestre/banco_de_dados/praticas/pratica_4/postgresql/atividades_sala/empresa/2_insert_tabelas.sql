7c80c8ccdfdd6aa3bba053d6ebbb41a6a322900c
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2018-05-25 20:48:04 -0300

    Adição de mais exemplos passados em sala

---
+++ 4_semestre/banco_de_dados/praticas/pratica_4/postgresql/atividades_sala/empresa/2_insert_tabelas.sql
@@ -0,0 +1,23 @@
+-- Inserindo departamentos
+INSERT INTO departamento VALUES (1, 'RH');
+INSERT INTO departamento VALUES (2, 'Vendas');
+INSERT INTO departamento VALUES (3, 'Informática');
+INSERT INTO departamento VALUES (4, 'Compras');
+
+-- Inserindo funcionários
+INSERT INTO funcionario VALUES (1, 'Paulo', 4000.00, 1);
+INSERT INTO funcionario VALUES (2, 'Maria', 1500.00, 2);
+INSERT INTO funcionario VALUES (3, 'João', 4000.00, 1);
+INSERT INTO funcionario VALUES (4, 'Laura', 5000.00, 2);
+INSERT INTO funcionario VALUES (5, 'Ana', 6000.00, 3);
+
+-- Inserido empregados (Tabela de auxílio a exemplos)
+INSERT INTO empregado VALUES (1, 'Ana', 2400);
+INSERT INTO empregado VALUES (2, 'Margarida', 1500);
+INSERT INTO empregado VALUES (3, 'Pedro', 1000);
+INSERT INTO empregado VALUES (4, 'Lia', 5000);
+
+-- Inserindo faixa salaria (Tabela de auxílio a exemplos)
+INSERT INTO faixa_salarial VALUES ('A', 1000, 2999);
+INSERT INTO faixa_salarial VALUES ('B', 3000, 5999);
+INSERT INTO faixa_salarial VALUES ('C', 6000, 9999);
